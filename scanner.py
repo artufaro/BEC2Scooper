9ee7cb1b70d9475233299580c86a8183b3f61bdf
barbierom <barbieromatteo91@gmail.com>
2019-09-03 13:51:13 +0200

    Added saving of scope traces to H5 file

--- scanner.py
+++ scanner.py
@@ -6,7 +6,9 @@ import json
 from ast import literal_eval
 from collections import defaultdict
 import time
-
+import yaml
+import zerorpc
+import numpy as np
 
 import h5py
 
@@ -19,6 +21,7 @@ from watchdog.events import PatternMatchingEventHandler
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG) 
 
 from cameras import cameras_db
 cameras_list = [cameras_db['StingrayHor']]#, cameras_db['HamamatsuVert']]
@@ -35,6 +38,7 @@ class Scooper:
         self.to_be_kept = True
         self.json_path = json_path
         self.cameras_list = cameras_list
+        self.scopes = []
 
 
     def remove_h5file(self, filepath):
@@ -65,7 +69,6 @@ class Scooper:
     
     def makeh5file(self):
         logger.info("shot started")
-
         
         now = datetime.now()
 
@@ -75,10 +78,11 @@ class Scooper:
         with open(self.json_path) as jsonfile:
            last_program = json.load(jsonfile)
            new_sequence_index = last_program["sequence_index"]
-         
-        with open('settings.py') as f:
-            save_dict = literal_eval(f.read())
-                
+        
+        # read current settings
+        with open('settings.yaml') as f:
+            save_dict = yaml.load(f)
+              
         
         self.to_be_kept = save_dict['save']
         # -------------------------------------------------------
@@ -122,10 +126,10 @@ class Scooper:
                    for k,v in last_program['variables'].items():
                        h5file["globals"].attrs[k] = v
             except  FileNotFoundError:
-                print("Json file not found")
+                logger.error("Json file not found")
             
             # add user globals
-            for k,v in save_dict['user globals'].items():
+            for k,v in save_dict['user_globals'].items():
                    h5file["globals"].attrs[k] = v
         
         # append file to the queue
@@ -137,7 +141,27 @@ class Scooper:
             try:
                 zprocess.zmq_get(42521, data=str(self.h5path), timeout=2)
             except zprocess.utils.TimeoutError:
-                print('runviewer is not ON')
+                logger.info('runviewer is not ON')
+        
+        # add new scopes and create clients
+        for scope in save_dict['scopes']:
+            # I know this is ugly
+            if scope['name'] not in [s['name'] for s in self.scopes]:
+                c = zerorpc.Client(scope['address'])
+                scope['client'] = c
+                scope['delayed_trg'] = False
+                self.scopes.append(scope)
+        
+        # arm all the scopes
+        for scope in self.scopes:
+        
+            # if it is not triggered, it means we are still running previous shot
+            if scope['client'].triggered() == False:
+                scope['delayed_trg'] = True
+            else:    
+                scope['client'].arm()
+                logger.info(f"arming scope {scope['name']} at: {scope['address']}")
+
     
     def gather_data(self):
         # -------------------------------------------------------
@@ -160,26 +184,42 @@ class Scooper:
         logger.info(f'Saving images to {_file}')
         
         with h5py.File(_file) as h5file:
+            
             # add raw images
             for c in self.cameras_list:
                 images = c.get_img()
                 
                 for k, v in images.items():
                     h5file[f"data/{c.name}/{k}"] = v
+            
+            # add scope traces
+            for scope in self.scopes:
+                # get_traces waits for the trigger to come
+                path = scope['client'].get_traces()
+                
+                # re-arm the scope if the next shot has started already 
+                if scope['delayed_trg']:
+                    scope['delayed_trg'] = False
+                    scope['client'].arm()
+                    logger.info(f"(delayed) arming scope {scope['name']} at: {scope['address']}")
+                    
+                # loading a .npz returns a dictionary-like object, but it needs closing!
+                with np.load(scope['filepath']) as data:
+                    for _name, _array in data.items():
+                        h5file[f"data/{scope['name']}/{_name}"] = _array
                     
 
         # -------------------------------------------------------
         # submit to lyse
-        with open('settings.py') as f:
-            save_dict = literal_eval(f.read())
+        with open('settings.yaml') as f:
+            save_dict = yaml.load(f)
             
         timeout = 2
         if save_dict['submit_to_lyse']:
             try:
                 self.submit_to_lyse(_file, timeout)
             except zprocess.utils.TimeoutError:
-                print('lyse is not ON')
-
+                logger.warning('lyse is not ON')
         print('END')
 
 
@@ -187,12 +227,9 @@ class Scooper:
 class Scanner:
     def __init__(self, files, path='.'):
     
-        # if it is int look for the number of files,
-        # otherwise a list of filenames should be provided explicitly
-        if not isinstance(files, int):
-            self.files = set(files)
-        else:
-            self.files = files
+        # provide explicitly a list of files, when all of them have been "moved"
+        # the data gathering is started
+        self.files = set(files)
         
         self._collected = []
         self.path = Path(path)
@@ -213,26 +250,46 @@ class Scanner:
         logger.info('Watching for files %s', self.files)
 
 
-    def _on_modified(self, event):
-        filename = Path(event.src_path).name
-            
-        if 'tmp' not in filename:
-            self._collected[0].add(filename)
-            logger.info('collected files now:' + str(self._collected))
-      
-        
-        
-        if self._collected[0] == self.files or len(self._collected[0]) == len(self.files):
-            self.scooper.gather_data()
-            self._collected.pop(0)
+#    def _on_modified(self, event):
+#        filename = Path(event.src_path).name
+#        
+#        logger.debug(event)
+#            
+#        if 'tmp' not in filename:
+#            self._collected[0].add(filename)
+#            logger.info('collected files now:' + str(self._collected))
+#      
+#        
+#        
+#        if self._collected[0] == self.files:
+#            self.scooper.gather_data()
+#            self._collected.pop(0)
         
     def _on_moved(self, event):
         """currently only the last_program.json is seen as moved file"""
         filename = Path(event.dest_path).name
         
+        logger.debug(event)
+        logger.info(f"collected {filename}")
+        
         if filename == 'last_program.json':
             self.scooper.makeh5file()
             self._collected.append({filename})
+        
+        else:
+            try:
+                if filename in self.files:
+                    self._collected[0].add(filename)
+                
+                    if self._collected[0] == self.files:
+                        self.scooper.gather_data()
+                        self._collected.pop(0)
+            
+            except IndexError as e:
+                raise(e)
+                logger.error(f"file {filename} has updated without any shot pending!")
+                self._collected.pop(0)
+        
 
     def scan(self):
         logger.info('Watchdog starting...')
@@ -249,6 +306,9 @@ if __name__ == '__main__':
     # Select which cameras are in use
     cameras_list = [cameras_db['StingrayHor']]#, cameras_db['HamamatsuVert']]
 
+    # only the files in monitoredfiles will be watched, and all the rest we assume 
+    # to be in place when this are updated. Place only vital files for the experiment
+    
     monitoredfiles = [c.files.name for c in cameras_list]
 
     scooper = Scooper('/home/gabriele/sis-fish/last_program.json',
